24fa99e94a00ff0d1ac300f657cec5ae87353b51
Akarsh Pandey <akarshpandey9599@gmail.com>
2022-06-29 19:43:22 +0530

    hehehe

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -13,95 +13,44 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
-import React from 'react';
+import React, {useState} from 'react'
 import { AiOutlineGoogle } from 'react-icons/ai';
 import { Link as NavLink } from 'react-router-dom';
-const Landing = () => {
-  return (
-    <Flex direction="column">
-      <Flex justifyContent={'flex-end'} mt={5} mr={5}>
-        <ColorModeSwitcher />
-      </Flex>
-      <Flex
-        direction={['column', 'column', 'column', 'row']}
-        minH={'75vh'}
-        align={'center'}
-        justifyContent={'space-evenly'}
-        bg={useColorModeValue('gray.50', 'gray.800')}
-      >
-        <Flex direction={'column'} m={10}>
-          <Heading
-            fontFamily={`'Source Code Pro', sans-serif`}
-            color={useColorModeValue('cyan.600', 'cyan')}
-            fontSize={['4xl', '4xl', '5xl', '6xl']}
-          >
-            {' '}
-            &lt;partnersInCrime&gt;
-          </Heading>{' '}
-        </Flex>
-        <Stack spacing={8} minW={['sm','md','md','md']} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading
-              fontSize={['3xl', '3xl', '4xl', '5xl']}
-              fontFamily={`'Source Code Pro',sans-serif`}
-            >
-              Sign in{' '}
-            </Heading>
-            <Text fontSize={['sm','md','md','lg']} color={'gray.600'}>
-              to start{' '}
-              <Text
-                display={'inline'}
-                color={useColorModeValue('cyan.600', 'cyan')}
-              >
-                Collaborating
-              </Text>{' '}
-              with your college mates 👨‍💻
-            </Text>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}
-          >
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input type="password" />
-              </FormControl>
-              <Stack spacing={5}>
-                <Stack
-                  direction={{ base: 'column', sm: 'row' }}
-                  align={'start'}
-                  justify={'center'}
-                >
-                  <Text>
-                    New User?{' '}
-                    <Link color={useColorModeValue('cyan.600', 'cyan')}>
-                      <NavLink to={'/signup'}> Sign Up</NavLink>
-                    </Link>
-                  </Text>
-                </Stack>
-                <Button colorScheme={'cyan'}>Sign in</Button>
-                <Flex align="center">
-                  <Divider />
-                  <Text padding="2">or</Text>
-                  <Divider />
-                </Flex>
-                <Button leftIcon={<AiOutlineGoogle />} colorScheme={'cyan'}>
-                  Sign in with Google
-                </Button>
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      </Flex>
-    </Flex>
-  );
-};
+import useAuth from '../context/AuthContext';
 
-export default Landing;
+function Landing() {
+
+ const {signInPopup} = useAuth()
+
+  return (
+      <Flex direction="column">
+          <Flex alignItems={'center'} justifyContent={'space-between'} mt={5} mr={5} ml={5}>
+              
+          <Flex direction={'column'}>
+          <Heading
+              fontFamily={`'Source Code Pro', sans-serif`}
+              color={useColorModeValue('cyan.600', 'cyan')}
+              fontSize={['20px', '24px', '28px', '30px']}
+          >
+              {' '}
+              &lt;partnersInCrime&gt;
+          </Heading>{' '}
+          </Flex>
+
+              <Flex justifyContent={'flex-end'} gap={3}>
+                  <Button colorScheme={'cyan'} onClick={signInPopup}>
+                      <AiOutlineGoogle />
+                  </Button>
+                  <ColorModeSwitcher/>
+              </Flex>
+          </Flex>
+
+          <Flex mt={5}>
+              hello
+          </Flex>
+
+      </Flex>
+  )
+}
+
+export default Landing
