571ba7d6de5ff2f5efb956c2cc6a6c83e12f3842
Ishaan Mahesh <ishaanmahesh20bcs54@iiitkottayam.ac.in>
2022-06-30 02:46:33 +0530

    profile pic update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,203 +9,231 @@ import {
     Textarea,
     useColorModeValue,
     useToast,
+    Image,
+    Box,
+    IconButton,
+    Icon
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import { FaImage } from 'react-icons/fa';
+import { FaImage, FaPencilAlt } from 'react-icons/fa';
 
 const Profile = () => {
-  let color = useColorModeValue('gray.900', 'gray.50');
-  const [image, setImage] = useState(null);
-  const [imageUrl, setImageUrl] = useState('');
-  const [uploading, setUploading] = useState(false);
-  const [imageError, setImageError] = useState('');
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [skills, setSkills] = useState('');
-  const toast = useToast();
-  let uploadImage = async () => {
-    setUploading(true);
-    if (image) {
-      setUploading(true);
-      let formData = new FormData();
-      formData.append('file', image);
-      try {
-        formData.append('upload_preset', 'partnersInCrime');
-        formData.append('cloud_name', 'dpjf6btln');
-        const res = await fetch(
-          'https://api.cloudinary.com/v1_1/dpjf6btln/image/upload',
-          {
-            method: 'POST',
-            body: formData,
-          }
-        );
-        if (res.status === 200) {
-          const data = await res.json();
-          setImageUrl(data.url);
-          setImageError('');
-          toast({
-            title: 'Image Uploaded Successfully',
-            status: 'success',
-            duration: 9000,
-            isClosable: true,
-          });
-        } else {
-          const data = await res.json();
-          setImageError(data.error);
-          console.log(data.error);
-        }
-      } catch (error) {
-        setImageError(error.message);
-        console.log(error.message);
-      } finally {
-        setUploading(false);
-        setImage(null);
-      }
-    } else {
-      setImageError('Please select an image');
-      setUploading(false);
-    }
-  };
-  useEffect(() => {
-    uploadImage();
-  }, [image]);
+    let color = useColorModeValue('gray.900', 'gray.50');
+    const [image, setImage] = useState(null);
+    const [imageUrl, setImageUrl] = useState('');
+    const [uploading, setUploading] = useState(false);
+    const [imageHover, setImageHover] = useState(false);
+    const [imageError, setImageError] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [skills, setSkills] = useState('');
+    const toast = useToast();
+    
+    useEffect(() => {
+        const userData = async () => {
+            setImageUrl("profile.png");
+        };
+        userData();
+    }, []);
 
-  return (
-    <Flex justifyContent={'center'} direction={'column'} align={'center'}>
-      <Heading fontFamily={`'Source Code Pro',sans-serif`}>
-        Got a Cool Idea 😏
-      </Heading>
-      <Text color={color} fontSize={'lg'} mt={5}>
-        {' '}
-        Let's make it Real 😎
-      </Text>
-      <Flex
-        mt={10}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        overflow={'hidden'}
-        p={8}
-        direction={'column'}
-        w={['sm', 'md', 'lg', 'xl']}
-      >
-        <Button
-          colorScheme={'teal'}
-          leftIcon={<FaImage />}
-          isLoading={uploading}
-        >
-          Add Image
-          <Input
-            type="file"
-            height="100%"
-            width="100%"
-            position="absolute"
-            top="0"
-            left="0"
-            opacity="0"
-            aria-hidden="true"
-            accept="image/*"
-            cursor={'pointer'}
-            disabled={uploading}
-            onChange={e => {
-              setImage(e.target.files[0]);
-            }}
-          />
-        </Button>
-      </Flex>
-      <Flex
-        mt={10}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        overflow={'hidden'}
-        p={8}
-        direction={'column'}
-        w={['sm', 'md', 'lg', 'xl']}
-      >
-        <Text
-          fontFamily={`'Source Code Pro',sans-serif`}
-          textAlign={'center'}
-          mb={4}
-        >
-          Well what's in the name, but still
-        </Text>
-        <FormControl>
-          <Input
-            placeholder="Your Project Name"
-            size={['sm', 'md', 'lg', 'lg']}
-            value={title}
-            onChange={e => setTitle(e.target.value)}
+    useEffect(() => {
+        const uploadImage = async () => {
             
-          />
-        </FormControl>
-      </Flex>
-      <Flex
-        mt={10}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        overflow={'hidden'}
-        p={8}
-        direction={'column'}
-        align={'center'}
-        w={['sm', 'md', 'lg', 'xl']}
-        justifyContent={'center'}
-      >
-        <Text
-          fontFamily={`'Source Code Pro',sans-serif`}
-          textAlign={'center'}
-          mb={4}
-        >
-          Billion Dollar Idea
-        </Text>
-        <FormControl>
-          <Textarea
-            placeholder="Your Idea"
-            rowGap={4}
-            boxSize={['xs', 'sm', 'md', 'lg']}
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-          />
-        </FormControl>
-      </Flex>
-      <Flex
-        mt={10}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        overflow={'hidden'}
-        p={8}
-        direction={'column'}
-        align={'center'}
-        w={['sm', 'md', 'lg', 'xl']}
-      >
-        <FormControl>
-          <Text
-            fontFamily={`'Source Code Pro',sans-serif`}
-            textAlign={'center'}
-            mb={4}
-          >
-            For your one hell of a team
-          </Text>
-          <Input
-            placeholder="Skills You Require"
-            size={['sm', 'md', 'lg', 'lg']}
-            value={skills}
-            onChange={e => setSkills(e.target.value)}
-          />
-        </FormControl>
-      </Flex>
-      <Flex m={10}>
-        <ButtonGroup spacing={10}>
-          <Button variant={'solid'} colorScheme={'teal'}>
-            Post Idea
-          </Button>
-          <Button variant={'solid'}>Save Draft</Button>
-        </ButtonGroup>
-      </Flex>
-    </Flex>
-  );
+            setUploading(true);
+    
+            if(image){
+                setUploading(true);
+                let formData = new FormData();
+                formData.append('file', image);
+    
+                try{
+                    formData.append('upload_preset', 'z4pkkeb2');
+                    formData.append('cloud_name', 'dikr8bxj7');
+                    const res = await fetch(
+                        'https://api.cloudinary.com/v1_1/dikr8bxj7/image/upload', {
+                            method: 'POST',
+                            body: formData,
+                    });
+                    if(res.status === 200){
+                        const data = await res.json();
+                        setImageUrl(data.url);
+                        setImageError('');
+
+                        toast({
+                            position: "bottom-right",
+                            title: 'Profile Pic Updated',
+                            status: 'success',
+                            duration: 9000,
+                            isClosable: true,
+                        });
+                    }
+                    else{
+                        const data = await res.json();
+                        setImageError(data.error);
+                        console.log(data.error);
+                    }
+                } 
+                catch(error){
+                    setImageError(error.message);
+                    console.log(error.message);
+                } 
+                finally{
+                    setUploading(false);
+                }
+            }
+            else{
+                setImageError('Please select an image');
+                setUploading(false);
+            }
+        };
+
+        uploadImage();
+    }, [image, toast]);
+
+    return (
+        <Flex direction={'column'}>
+            <Flex justifyContent = {'space-evenly'} direction = {['column', 'row']} align = {'center'} my = {["0", "2"]}>
+                <Heading fontFamily={`'Source Code Pro',sans-serif`} size = {["2xl", "xl"]} my = {["7", "0"]}>
+                    Who are you? 🤔	
+                </Heading>
+                <label for = "picId"><Box
+                    position = {'relative'}
+                    borderRadius = {'full'}
+                    onMouseEnter = {() => setImageHover(true)}
+                    onMouseLeave = {() => setImageHover(false)}
+                    cursor = {imageHover ? 'pointer' : 'auto'}
+                    borderColor = {'teal'}
+                    borderWidth = {3}
+                >
+                    {imageHover && 
+                        <>
+                            <IconButton 
+                                position = {'absolute'}
+                                top = {'40%'}
+                                left = {'40%'}
+                                colorScheme='teal'
+                                cursor = {'pointer'}
+                                icon = {<FaPencilAlt />}
+                                isLoading = {uploading}
+                            />
+                            <Input
+                                id = "picId"
+                                type = "file"
+                                display = {'none'}
+                                aria-hidden="true"
+                                accept="image/*"
+                                cursor={'pointer'}
+                                disabled={uploading}
+                                onChange={e => {
+                                    setImage(e.target.files[0]);
+                                }}
+                            /> 
+                        </>
+                        
+                    }
+                    <Image
+                        _hover = {{opacity: '0.1'}}
+                        borderRadius = 'full'
+                        boxSize = {['200px', '170px']}
+                        src = {imageUrl}
+                        alt = 'Profile Pic'
+                        zIndex = {-1}
+                    />
+                </Box></label>
+            </Flex>
+            <Flex
+              mt={10}
+              bg={useColorModeValue('white', 'gray.900')}
+              boxShadow={'2xl'}
+              rounded={'md'}
+              overflow={'hidden'}
+              p={8}
+              direction={'column'}
+              w={['sm', 'md', 'lg', 'xl']}
+            >
+              <Text
+                fontFamily={`'Source Code Pro',sans-serif`}
+                textAlign={'center'}
+                mb={4}
+              >
+                Well what's in the name, but still
+              </Text>
+              <FormControl>
+                <Input
+                  placeholder="Your Project Name"
+                  size={['sm', 'md', 'lg', 'lg']}
+                  value={title}
+                  onChange={e => setTitle(e.target.value)}
+                
+                />
+              </FormControl>
+            </Flex>
+            <Flex
+              mt={10}
+              bg={useColorModeValue('white', 'gray.900')}
+              boxShadow={'2xl'}
+              rounded={'md'}
+              overflow={'hidden'}
+              p={8}
+              direction={'column'}
+              align={'center'}
+              w={['sm', 'md', 'lg', 'xl']}
+              justifyContent={'center'}
+            >
+              <Text
+                fontFamily={`'Source Code Pro',sans-serif`}
+                textAlign={'center'}
+                mb={4}
+              >
+                Billion Dollar Idea
+              </Text>
+              <FormControl>
+                <Textarea
+                  placeholder="Your Idea"
+                  rowGap={4}
+                  boxSize={['xs', 'sm', 'md', 'lg']}
+                  value={description}
+                  onChange={e => setDescription(e.target.value)}
+                />
+              </FormControl>
+            </Flex>
+            <Flex
+              mt={10}
+              bg={useColorModeValue('white', 'gray.900')}
+              boxShadow={'2xl'}
+              rounded={'md'}
+              overflow={'hidden'}
+              p={8}
+              direction={'column'}
+              align={'center'}
+              w={['sm', 'md', 'lg', 'xl']}
+            >
+              <FormControl>
+                <Text
+                  fontFamily={`'Source Code Pro',sans-serif`}
+                  textAlign={'center'}
+                  mb={4}
+                >
+                  For your one hell of a team
+                </Text>
+                <Input
+                  placeholder="Skills You Require"
+                  size={['sm', 'md', 'lg', 'lg']}
+                  value={skills}
+                  onChange={e => setSkills(e.target.value)}
+                />
+              </FormControl>
+            </Flex>
+            <Flex m={10}>
+              <ButtonGroup spacing={10}>
+                <Button variant={'solid'} colorScheme={'teal'}>
+                  Post Idea
+                </Button>
+                <Button variant={'solid'}>Save Draft</Button>
+              </ButtonGroup>
+            </Flex>
+        </Flex>
+    );
 };
 
 export default Profile;
